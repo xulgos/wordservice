f4a3322bda6c69edcafa630446da976c28984599
rubedo <rubedoje@gmail.com>
2013-03-09 00:44:45 -0400

    Tonight... WE CODE IN HELLjruby -rubygems -I.:lib:test test/test_dictionary.rb ! Tried using mongomapper and failed. Tried to use mongoid and Succeded (in ruby).

--- test/helper.rb
+++ test/helper.rb
@@ -1,9 +1,13 @@
 ENV['RACK_ENV'] = 'test'
 gem 'minitest'
+require 'sinatra'
 require 'minitest/autorun'
 require 'rack/test'
+require 'mongoid'
 
 include Rack::Test::Methods
 def app
   WordResource
 end
+
+Mongoid.sessions = {default: {hosts: ['localhost:27017'], database: 'test'}}
--- test/test_dictionary.rb
+++ test/test_dictionary.rb
@@ -1,18 +1,20 @@
-require 'minitest/autorun'
+require 'helper'
 require 'dictionary'
 
 describe Dictionary do
 
   before do
-    @file = File.open('dictionaries/en_US.dic')
-    @dictonary = Dictionary.new(@file)
+    %w[sure right that is not how you write it I see still wrong].each do |word|
+      Dictionary.create(word: word).save
+    end
   end
 
   after do
-    @file.close
+    Dictionary.destroy_all
   end
 
   it "should be able to verify that word exists" do
-    @dictonary.must_include('dictonary')
+    queried_word = Dictionary.find_by(word: 'right')
+    queried_word.word.must_equal 'right'
   end
 end
