b6b5e9b81cab2b27751e62750774e2517609824e
Sujemu <sujemu@gmail.com>
2013-03-29 22:49:15 -0400

    changed to regular ruby only

--- lib/wordresource.rb
+++ lib/wordresource.rb
@@ -1,14 +1,19 @@
 require 'sinatra/base'
+require 'sinatra/json'
 require 'json'
 
 class WordResource < Sinatra::Base
+  helpers JSON::helpers
 
   def initialize(word_smugler)
     @word_smugler = word_smugler
   end
 
   get '/word' do
-    result = {word: @word_smugler.random}
-    JSON.generate(result)
+    json word: @word_smugler.random
+  end
+
+  get '/word/:anyword' do
+    json word: @word_smugler.get(params[:anyword])
   end
 end
--- test/test_wordresource.rb
+++ test/test_wordresource.rb
@@ -5,25 +5,25 @@ require 'json'
 
 describe WordResource do
 
+  before do
+    @smuggler = WordSmuggler.new
+    TestHelpers::fill_word_collection
+  end
+
+  after do
+    TestHelpers::clear_word_collection
+  end
+
+  def app
+    WordResource.new(@smuggler)
+  end
+
+  def get_and_parse_response(word=nil)
+    get word ? "/word/#{word}" : '/word'
+    JSON.parse(last_response.body)
+  end
+
   describe "When accessed with a get to /word" do
-
-    before do
-      @smuggler = WordSmuggler.new
-      TestHelpers::fill_word_collection
-    end
-
-    after do
-      TestHelpers::clear_word_collection
-    end
-
-    def app
-      WordResource.new(@smuggler)
-    end
-
-    def get_and_parse_response
-      get '/word'
-      JSON.parse(last_response.body)
-    end
 
     it "won't return an empty response" do
       get '/word'
@@ -40,6 +40,19 @@ describe WordResource do
         lambda { |results| results << get_and_parse_response },
         lambda { |result| result['word'] }
       )
+    end
+  end
+
+  describe "When accessed with a get to /word/{anyword}" do
+
+    it "returns a the resource of a given word" do
+      word = get_and_parse_response('fog')
+      word.value.must_equal('fog')
+    end
+
+    it "if the word does not exist it returns a 404" do
+      get '/word/doesnotexist'
+      last_response.status.must_equal 404
     end
   end
 end
